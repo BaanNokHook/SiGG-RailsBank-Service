4a444af9248e9762a243686fcf3a8d3a40650791
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-27 17:30:50 +0700

    SiGG-RailsBank-Service-app-controllers-accounts_controller

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,10 @@
+class AccountsController < ApplicationController
+      before_action :require_user
+    
+      def show
+        @user = User.find(params[:user_id])
+        @account = Account.find(params[:account_id])
+        @transactions = @user.transactions.where(account_id: params[:account_id])
+      end
+    end
+    
